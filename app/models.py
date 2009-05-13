342d76b9dec46b3e83c8cee31f8cc9c7e63f8ddb
russell_h <russellhaering@gmail.com>
2009-05-12 18:54:25 -0700

    Fix a problem that broke manage.py, switch to DecimalField for currency

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class ExpenseType(models.Model):
 
 class MicroBill(models.Model):
     applies_to = models.ForeignKey(User)
-    amount = models.IntegerField()
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
 
 class Expense(models.Model):
     desc = models.CharField(max_length=128)
@@ -20,58 +20,6 @@ class Expense(models.Model):
         return self.desc
     type = models.ForeignKey(ExpenseType)
     issue_date = models.DateField()
-    amount = models.IntegerField()
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
     bills = models.ManyToManyField(MicroBill)
     added_by = models.ForeignKey(User)
-
-# Forms
-class BillerWidget(forms.MultiWidget):
-    def __init__(self, victims):
-        self.victims = victims
-        widgets = ((forms.TextInput()) for victim in victims)
-        super(BillerWidget, self).__init__(widgets)
-
-    def decompress(self, values):
-        if values:
-            return values
-        else:
-            return list((None) for widget in self.widgets)
-
-    def format_output(self, rendered_widgets):
-        output = u'<table>'
-        for i, widget in enumerate(rendered_widgets):
-            output = output + "<tr><th>%s</th><td>%s</td></tr>\n" % (self.victims[i], widget)
-        return output + "</table>" 
-
-class BillerField(forms.RegexField):
-    currencyRe = re.compile(r'^[0-9]{1,5}(.[0-9][0-9])?$')
-    error_messages = {
-        'invalid': u'Enter a valid USD amount',
-    }
-    def __init__(self, victims):
-        self.victims = victims
-        self.widget = BillerWidget(victims)
-        super(BillerField, self).__init__(self.currencyRe, error_messages=self.error_messages)
-
-    def _clean_value(self, value):
-        value = super(BillerField, self).clean(value)
-        return float(value)
-
-    def clean(self, values):
-        errors = ErrorList()
-        cleaned_values = []
-        for value in values:
-            try:
-                cleaned_values.append(self._clean_value(value))
-            except ValidationError, e:
-                errors.extend(e.messages) 
-        if errors:
-            raise ValidationError(errors)
-        return cleaned_values
-
-class ExpenseForm(forms.Form):
-    type = forms.ModelChoiceField(queryset=ExpenseType.objects.all())
-    desc = forms.CharField(max_length=128)
-    amount = forms.FloatField()
-    issue_date = forms.DateField()
-    bills = BillerField(User.objects.all())
